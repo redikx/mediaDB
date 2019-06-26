b2232c057c69ec4a4758693ff8fd1708ad3e27f2
redik <redikx@gmail.com>
2019-06-26 23:03:10 +0200

    get_film_details added, function empty

--- imdb_get_info.py
+++ imdb_get_info.py
@@ -2,7 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_possible_films_list(film_title):
+def get_possible_films_list(film_title: str) -> []:
+    """
+    Function that generates list of all possible films matches input strin
+    :param film_title: string searching, white spaces are then converted to +
+    :return: list [ tt, title ]
+    """
     film_search = film_title.replace(' ', '+')
     url_movie = 'https://www.imdb.com/find?ref_=nv_sr_fn&q=' + film_search + '&s=tt&ref_=fn_al_tt_mr'
     resp = requests.get(url_movie)
@@ -16,10 +21,10 @@ def get_possible_films_list(film_title):
                 and '(Short)' not in str(i) and '(Video)' not in str(i) \
                 and '(TV Series)' not in str(i) and '(in development)' not in str(i)\
                 and '(Video Game)' not in str(i) and '(TV Movie)' not in str(i)\
-                and film_title in str(i):
-            print("Processing : " + str(i))
-            print(str(i).split(">")[1].strip('<a href="/title/'))
-            print(str(i).split(">")[2].strip("</a"))
+                and 'Documentary' not in str(i) and film_title in str(i):
             movie_list.append([str(i).split(">")[1].strip('<a href="/title/'), str(i).split(">")[2].strip("</a")])
-            print("*" * 40)
     return movie_list
+
+def get_film_details(tt):
+    url_movie = 'https://www.imdb.com/title/tt' + str(tt) + '/?ref_=fn_tt_tt_1'
+    print(url_movie)
