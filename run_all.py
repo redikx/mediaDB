8dea131f4352bedac64cd865eb5f9db05e3e1b96
redik <redikx@gmail.com>
2019-07-08 18:38:54 +0200

    finished interactive : ask for title, search, printed if exact match, otherwise shows list found and ask for choosing which one is correct.

--- run_all.py
+++ run_all.py
@@ -1,7 +1,8 @@
 from imdb_get_info import get_possible_films_list
 from imdb_get_info import get_film_details_page
 
-title_search = "Assassin"
+title_search = input("Enter movie title to search : ")
+#title_search = "Assassin"
 fl = get_possible_films_list(title_search)
 found_exact = 0
 for i in fl:
@@ -14,12 +15,20 @@ for i in fl:
 
 if found_exact == 0:
     num_on_list = 0
-    print("No match, want to select from list?")
+    print("No match, select from list?")
     for j in fl:
-        print(str(num_on_list) + ": " + str(j))
+        movie_temp = get_film_details_page(j[0], j[1])
+        row = str(num_on_list) + " Title : " + str(j[1]) + " ; Director : " + str(movie_temp.director) + " ; Year : " + str(movie_temp.year)
+        print(row)
         num_on_list+=1
     #TODO : prompt to enter list number, then return movie
+    chosen = input(">>")
+
+    chosen_film = fl[int(chosen)]
+    chosen_film_det = get_film_details_page(fl[int(chosen)][0], fl[int(chosen)][1])
+    print(chosen_film_det)
 print('*'*100)
+
 
 
 
